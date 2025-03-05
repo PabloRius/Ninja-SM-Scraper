d60dc4c4252db350427ff9ddeec81d9aba79dee1
PabloRius <pablogrius@gmail.com>
2025-03-05 09:46:51 +0000

    Fix pare_many bug on empty page, add debug message

--- src/scraper/parser.py
+++ src/scraper/parser.py
@@ -1,9 +1,17 @@
 """Parsing logic"""
-from typing import List, Union, Callable
 
-def parse_many(pages:list[str|None], parse_one:Callable[[Union[str, None]], List])->List:
-    """Extract product details from multiple HTML pages"""
+from typing import List, Union, Callable
+from common.utils import debug
+
+def parse_many(pages: list[Union[str, None]], parse_one: Callable[[Union[str, None]], List]) -> List:
+    """Extract product details from multiple HTML pages."""
+
+    total = len(pages)
     products = []
-    for page in pages:
-        products.extend(parse_one(page))
+
+    for index, page in enumerate(pages, start=1):
+        debug(f"📄 Parsing page {index}/{total} ({(index/total) * 100:.2f}%)")
+        parsed_products = parse_one(page) if page else []
+        products.extend(parsed_products)
+
     return products
